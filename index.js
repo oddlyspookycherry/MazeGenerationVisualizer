79031748daa65604ad22132b2f041f44d8b0c6b4
oddlyspookycherry <decentblack0@gmail.com>
2022-08-25 02:28:36 -0400

    method selection

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 "use strict";
+
 const mazegrid = document.getElementById('mazegrid');
 const generate_btn = document.getElementById('generate btn');
 const nrowsinput = document.getElementById('n rows');
 const ncolsinput = document.getElementById('n cols');
+const methodinput = document.getElementById('method select');
 
 function sleep(duration_ms) {
     return new Promise(resolve => setTimeout(resolve, duration_ms));
@@ -13,6 +15,11 @@ let maze_arr = null;
 let anim_delay = 50;
 let in_generation = false;
 
+const CellType = {
+    passage: "passage",
+    wall: "wall"
+};
+
 function init_maze(nrows, ncols) {
     mazegrid.innerHTML = '';
     mazegrid.style.gridTemplate = `repeat(${nrows}, 1fr) / repeat(${ncols}, 1fr)`;
@@ -22,31 +29,54 @@ function init_maze(nrows, ncols) {
     }
 }
 
-function create_cell(row, col) {
+function fill_cell(row, col, type) {
+    maze_arr[row][col] = type;
+
     const cell = document.createElement('div');
-    cell.classList.add('mazecell', 'passage');
+    cell.classList.add('mazecell', type);
     cell.style.gridArea = `${row + 1} / ${col + 1} / span 1 / span 1`;
     mazegrid.appendChild(cell);
-    maze_arr[row, col] = cell;
 }
 
-async function generate_maze(nrows, ncols) {
-    in_generation = true;
+async function generate_maze(nrows, ncols, generation_method) {
     init_maze(nrows, ncols);
-    for (let r = 0; r < nrows; r++) {
-        for (let c = 0; c < ncols; c++) {
-            create_cell(r, c);
-            await sleep(anim_delay);
-        }
-    }
-    in_generation = false;
+    generation_method(nrows, ncols);
 }
+
+const gen_methods = {
+    "all_passages": async function (nrows, ncols) {
+        in_generation = true;
+        for (let r = 0; r < nrows; r++) {
+            for (let c = 0; c < ncols; c++) {
+                fill_cell(r, c, CellType.passage);
+                await sleep(anim_delay);
+            }
+        }
+        in_generation = false;
+    },
+    "all_walls": async function (nrows, ncols) {
+        in_generation = true;
+        for (let r = 0; r < nrows; r++) {
+            for (let c = 0; c < ncols; c++) {
+                fill_cell(r, c, CellType.wall);
+                await sleep(anim_delay);
+            }
+        }
+        in_generation = false;
+    },
+    "randomized_depth_first_search": async function (nrows, ncols) {
+        in_generation = true;
+        in_generation = false;
+    }
+};
 
 // main script
 init_maze(10, 10);
 
+// TO-DO: change button color while animation playing
 generate_btn.addEventListener('click', () => {
-    let nrows = nrowsinput.value;
-    let ncols = ncolsinput.value;
-    if (!in_generation) { generate_maze(nrows, ncols); }
+    let nrows = parseInt(nrowsinput.value);
+    let ncols = parseInt(ncolsinput.value);
+    let method_name = methodinput.value;
+    if (!in_generation) { generate_maze(nrows, ncols, gen_methods[method_name]); }
 });
