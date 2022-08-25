b4b66be215a3cf304a7d3b69f305bbfac903e71f
oddlyspookycherry <decentblack0@gmail.com>
2022-08-25 00:09:41 -0400

    complete maze framework

--- index.js
+++ index.js
@@ -3,13 +3,19 @@ const mazegrid = document.getElementById('mazegrid');
 const generate_btn = document.getElementById('generate btn');
 const nrowsinput = document.getElementById('n rows');
 const ncolsinput = document.getElementById('n cols');
-const animspeedinput = document.getElementById('anim speed');
+
+function sleep(duration_ms) {
+    return new Promise(resolve => setTimeout(resolve, duration_ms));
+}
 
 // declarations
 let maze_arr = null;
-let init_anim_speed = 100;
+let anim_delay = 50;
+let in_generation = false;
 
-function init_maze_arr(nrows, ncols) {
+function init_maze(nrows, ncols) {
+    mazegrid.innerHTML = '';
+    mazegrid.style.gridTemplate = `repeat(${nrows}, 1fr) / repeat(${ncols}, 1fr)`;
     maze_arr = new Array(nrows);
     for (let i = 0; i < maze_arr.length; i++) {
         maze_arr[i] = new Array(ncols);
@@ -19,33 +25,28 @@ function init_maze_arr(nrows, ncols) {
 function create_cell(row, col) {
     const cell = document.createElement('div');
     cell.classList.add('mazecell', 'passage');
+    cell.style.gridArea = `${row + 1} / ${col + 1} / span 1 / span 1`;
     mazegrid.appendChild(cell);
     maze_arr[row, col] = cell;
 }
 
-function sleep(duration_ms) {
-    return new Promise(resolve => setTimeout(resolve, duration_ms));
-}
-
-async function generate_maze(nrows, ncols, anim_speed_multiplier) {
-    init_maze_arr(nrows, ncols);
-    mazegrid.innerHTML = '';
-
-    mazegrid.style.gridTemplate = `repeat(${nrows}, 1fr) / repeat(${ncols}, 1fr)`;
+async function generate_maze(nrows, ncols) {
+    in_generation = true;
+    init_maze(nrows, ncols);
     for (let r = 0; r < nrows; r++) {
         for (let c = 0; c < ncols; c++) {
             create_cell(r, c);
-            await sleep(init_anim_speed * 100 / anim_speed_multiplier);
+            await sleep(anim_delay);
         }
     }
+    in_generation = false;
 }
 
-// script
-mazegrid.innerHTML = '';
+// main script
+init_maze(10, 10);
 
 generate_btn.addEventListener('click', () => {
     let nrows = nrowsinput.value;
     let ncols = ncolsinput.value;
-    let anim_speed_multiplier = animspeedinput.value;
-    generate_maze(nrows, ncols, anim_speed_multiplier);
+    if (!in_generation) { generate_maze(nrows, ncols); }
 });
